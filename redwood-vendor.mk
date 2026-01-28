0a762a0f5344510339302b3df921ada2d875d290
Jaideep Sharma <jaidshar@codeaurora.org>
2026-01-28 11:24:26 +0000

    redwood: audio: Remove shoebox effect This effect is no longer supported, hence removing it.
    Change-Id: I9636c69a8257dd453193cb0f0f7087e2bb9dd6d6

--- redwood-vendor.mk
+++ redwood-vendor.mk
@@ -546,7 +546,6 @@ PRODUCT_PACKAGES += \
     libwfduibcsrcinterface_proprietary \
     libwfdutils_proprietary \
     libasphere \
-    libshoebox \
     vendor.qti.hardware.wifidisplaysession@1.0_vendor \
     vendor.qti.hardware.wifidisplaysessionl@1.0-halimpl \
     libxlog \
